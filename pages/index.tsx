4032a656201d59e7634be7abaaa3a5e929d5694e
Devergence <PrikhodkoDG@akbarsdigital.ru>
2022-01-13 20:59:34 +0500

    Add products

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import {Button, Htag, Rating} from "../components";
 import {useState} from "react";
 import {withLayout} from "../layout/Layout";
+import {GetStaticProps} from "next";
+import axios from "axios";
+import {MenuItem} from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu, firstCategory }: HomeProps): JSX.Element {
   const [rating, setRating] = useState(3);
   return (
     <>
@@ -10,8 +13,29 @@ function Home(): JSX.Element {
       <Button appearance='primary' arrow='down'>Кнопка</Button>
       <Button appearance='ghost'>Кнопка</Button>
       <Rating rating={rating} isEditable setRating={setRating}/>
+     <ul>
+       {menu.map(m => <li key={m._id.secondCategory}>{m._id.secondCategory}</li>)}
+     </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory= 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUNLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+interface HomeProps extends Record<string, unknown>{
+  menu: MenuItem[];
+  firstCategory: number;
+}
